0efecd65b863c50e65ae1a49f82c0b8ac359070d
Charlie Llewellin <charlie@llewellin.net>
2017-12-15 15:29:31 -0500

    zenfolio

--- etype_zenfolio.js
+++ etype_zenfolio.js
@@ -1,28 +1,36 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+(function ($) {
+    Drupal.behaviors.etype_zenfolio = {
+        attach: function (context) {
+            var slideIndex = 1;
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+            // Next/previous controls
+            function plusSlides(n) {
+                showSlides(slideIndex += n);
+            }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+            // Thumbnail image controls
+            function currentSlide(n) {
+                showSlides(slideIndex = n);
+            }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("zenfolio-slides");
-    var dots = document.getElementsByClassName("zenfolio-dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+            function showSlides(n) {
+                var i;
+                var slides = $(".zenfolio-slides");
+                var dots = $(".zenfolio-dot");
+                if (n > slides.length) { slideIndex = 1 }
+                if (n < 1) { slideIndex = slides.length }
+                for (i = 0; i < slides.length; i ++) {
+                    slides[i].css("display", "none");
+                }
+                for (i = 0; i < dots.length; i ++) {
+                    dots[i].removeClass(" active");
+                }
+                slides[slideIndex -1].css("display", "block");
+                dots[slideIndex -1].addclass("active");
+            }
+
+            showSlides(slideIndex);
+        }
+    };
+})(jQuery);
+
